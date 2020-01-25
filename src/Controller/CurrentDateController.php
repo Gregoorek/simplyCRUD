be26f144972bb7eaeb4ab0b7962a60e362746fc8
Grzegorz Grudzień <grzes1987@gmail.com>
2020-01-25 19:32:15 +0100

    routing zaawansowany z metodami post get

--- src/Controller/CurrentDateController.php
+++ src/Controller/CurrentDateController.php
@@ -9,27 +9,40 @@ namespace App\Controller;
     class CurrentDateController
     {
         /**
-         * @Route(path="/index",name="curentDate")
+         * @Route(path="/index",name="curentDate", methods={"POST"})
          * @return Response
          * @throws
          */
         public function curentDate() :Response
          {
             $currentDate = new \DateTime();
-            $currentDateFormat = $currentDate->format(DATE_ATOM);
-            $html = <<< EOT
-            <html>
+            return $this->getDataResponse('current date ',$currentDate);
+        }
+
+
+        /**
+         * @Route(path="/index", methods={"GET"})
+         * @return Response
+         * @throws \Exception
+         */
+        public function tommorowDate(): Response
+        {
+            $tommorow = new \DateTime();
+            $tommorow->add(new \DateInterval('P1D'));
+            return  $this->getDataResponse('tommorow date',$tommorow);
+        }
+
+        private function getDataResponse(string $title , \DateTime $dateTime)
+        {
+            $format = $dateTime->format(DATE_ATOM);
+             $html = <<< EOT
+            <html lang="pl">
             <body>
-                <h1>curent data</h1>
-                <p>$currentDateFormat</p>
+                <h1>$title</h1>
+                <p>$format</p>
             </body>
             </html>
 EOT;
-
-
             return new Response($html);
-
-
         }
-
     }
