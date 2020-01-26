a78d4250fc4dc1d11be35897ed53ef7dc3492228
Grzegorz Grudzień <grzes1987@gmail.com>
2020-01-26 01:36:20 +0100

    dalej nie dziala

--- src/Controller/helloController.php
+++ src/Controller/helloController.php
@@ -11,12 +11,29 @@ use Symfony\Component\HttpFoundation\Response;
 class helloController extends AbstractController
 {
     /**
-     * @Route(path="/index", methods={"GET"})
+     * @Route(path="/index", methods={"GET"}, name="hello")
      * @param string $name
-     * @param
+     * @param Request $request
      * @return Response
      * @throws \Exception
      */
 
+    public function hello(string $name ,Request $request):Response
+    {
+        $helloText = "czesc ".$name;
+        $param1 = $request->get('param1', 'jakas wiadomosc');
+        return new Response("<html lang='pl'><body><h1>$helloText</h1><p>$param1</p></body></html>");
+    }
 
+    /**
+     * @Route(path="/redirect/{action}", requirements={"hello|currentDate"} , name="hello2")
+     * @param string $action
+     * @return RedirectResponse
+     * @throws \Exception
+     */
+
+    public function moceToAction(string $action):RedirectResponse
+    {
+        return $this->redirectToRoute($action, ['name'=>'some name']);
+    }
 }
