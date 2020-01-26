9190635ce87e763d9a0afa38f27d77156d4a7623
Grzegorz Grudzień <grzes1987@gmail.com>
2020-01-26 01:08:04 +0100

    napisanie od nowa

---
+++ src/Controller/helloController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class helloController extends AbstractController
+{
+    /**
+     * @Route(path="/index", methods={"GET"})
+     * @param string $name
+     * @param
+     * @return Response
+     * @throws \Exception
+     */
+
+
+}
